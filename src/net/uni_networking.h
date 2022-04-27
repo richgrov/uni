d4fc78474ffb648779d33a3801a81efacc08153e
Richard Grover <38992201+richgrov@users.noreply.github.com>
2022-04-27 16:08:53 -0600

    Merge uni_uring.h into uni_networking.h

--- src/net/uni_networking.h
+++ src/net/uni_networking.h
@@ -1,10 +1,30 @@
 #ifndef UNI_NETWORKING_H
 #define UNI_NETWORKING_H
 
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "uni.h"
 #include "uni_os_constants.h"
 
 #ifdef UNI_OS_LINUX
-#include "uni_uring.h"
+#include "liburing.h"
+#include <netinet/in.h>
 #endif // UNI_OS_LINUX
+
+typedef struct {
+    UniServer *server;
+
+#ifdef UNI_OS_LINUX
+    struct io_uring ring;
+    int fd;
+    struct sockaddr_in server_addr;
+    socklen_t addr_len;
+#endif // UNI_OS_LINUX
+} UniNetworking;
+
+bool uni_net_init(UniNetworking *net, uint16_t port, UniError *err);
+
+bool uni_net_run(UniNetworking *net);
 
 #endif // UNI_NETWORKING_H
--- src/net/uni_uring.c
+++ src/net/uni_uring.c
@@ -1,4 +1,4 @@
-#include "uni_uring.h"
+#include "uni_networking.h"
 
 #include <stdlib.h>
 #include <string.h>
