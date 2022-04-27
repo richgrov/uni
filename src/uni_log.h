e371b328ab518235e3db19b39fd798ae47cfdc82
Richard Grover <38992201+richgrov@users.noreply.github.com>
2022-04-26 22:06:35 -0600

    Add more tools to uni_log.h

--- src/uni_log.h
+++ src/uni_log.h
@@ -2,7 +2,16 @@
 #define UNI_LOG_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #define UNI_LOG(fmt, ...) printf(fmt "\n", __VA_ARGS__)
+
+#define UNI_UNREACHABLE() UNI_LOG("UNREACHABLE(): %s:%d", __func__, __LINE__); abort()
+
+#ifdef UNI_DEBUG
+#define UNI_DLOG(fmt, ...) UNI_LOG(fmt, __VA_ARGS__)
+#else // UNI_DEBUG
+#define UNI_DLOG
+#endif // !UNI_DEBUG
 
 #endif // UNI_LOG_H
